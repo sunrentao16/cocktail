e512324ef90dc83c293bbbba4a51678b1abf6ac7
Dan <sunrentao@gmail.com>
2017-05-28 00:17:50 -0700

    nutrition added

--- get_data.py
+++ get_data.py
@@ -59,21 +59,53 @@ def get_recipe(url):
 	#print(rating_vote, '\n')
 	
 	# nutrition
-	nutrition = soup.findAll(class_ = 'l1a')[2:]
-	print(len(nutrition))
-	print(nutrition,'\n')
-	#print(name)
+	nutrition = soup.findAll(class_ = 'l1a')[2:] # get table
+	nutri_dict = {} # initialize
+	## column 1
+	cl1 = [x for x in nutrition[0].text.split() if '(' not in x]
+	print(cl1)
+	
+	## column 2
+	cl2_raw =  [None]*4# 2nd row is missing
+	for index, x in enumerate(nutrition[1].findAll('span')):
+		cl2_raw[index]= x.text
+	### find 2nd row
+	tmp_str = nutrition[1].text
+	row_2 = tmp_str[len(cl2_raw[0]):tmp_str.find(cl2_raw[1])]
+	cl2_raw.insert(1,row_2)
+	
+	## column 3
+	cl3 = nutrition[2].text.split()
+	print(cl3)
+	
+	## column 4
+	cl4_raw =  [None]*4# 5nd row is missing
+	for index, x in enumerate(nutrition[3].findAll('span')):
+		cl4_raw[index]= x.text
+	### find 5nd row
+	tmp_str = nutrition[3].text.strip()
+	row_5 = tmp_str[tmp_str.find(cl4_raw[3])+len(cl4_raw[3]):]
+	cl4_raw.append(row_5)
+	
+	for index, x in enumerate(cl1):
+		nutri_dict[x] = cl2_raw[index]
+	for index, x in enumerate(cl3):
+		nutri_dict[x] = cl4_raw[index]
+	
+	for key, v in nutri_dict.items():
+		print(key, ':', v)
+	#print(cl2)
 	for key, v in recipe.items():
 		print(key, ':', v)
-
-#url = 'https://www.bigoven.com/recipe/pomegranate-key-lime-vodka-cocktails/641724'
-#url = 'https://www.bigoven.com/recipe/the-old-fashioned-cocktail/124600'
-
-url = 'http://www.drinksmixer.com/'
-url = 'http://www.drinksmixer.com/drink5056.html'
-url = 'http://www.drinksmixer.com/drink2961.html'
-url = 'http://www.drinksmixer.com/drink3954.html'
-get_recipe(url)
+		
+	return(recipe, nutri_dict)
+	
+#	
+#url = 'http://www.drinksmixer.com/'
+#url = 'http://www.drinksmixer.com/drink5056.html'
+#url = 'http://www.drinksmixer.com/drink2961.html'
+#url = 'http://www.drinksmixer.com/drink3954.html'
+#get_recipe(url)
 
 
 

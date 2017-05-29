0f54717381e4d2c2b1279b914ba525e4c5737682
Dan <sunrentao@gmail.com>
2017-05-28 19:45:58 -0700

    clean data function added 1st time

---
+++ clean_data.py
@@ -0,0 +1,16 @@
+# clean data
+import pickle
+
+# read data
+def clean_data():
+
+	with open('./data/cocktail_data_124', 'rb') as fp:
+		data = pickle.load(fp)
+	
+	with open('./data/cocktail_data_raw', 'rb') as fp:
+		data_2 = pickle.load(fp)
+	
+	
+	print(len(data))
+	print(len(data_2))
+clean_data()

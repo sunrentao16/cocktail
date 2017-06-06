0b76752f41e6de73351a96f3c4649593c7ba7ed4
dan <sunrentao@gmail.com>
2017-06-05 23:56:53 -0700

    get variables names of each recipes

--- clean_data.py
+++ clean_data.py
@@ -1,17 +1,31 @@
 # clean data
-import pickle
-
+try:
+   import cPickle as pickle
+except:
+   import pickle
 # read data
 def clean_data():
 
-	with open('./data/cocktail_data_124', 'rb') as fp:
-		data = pickle.load(fp)
-	
 	with open('./data/cocktail_data_raw', 'rb') as fp:
-		data_2 = pickle.load(fp)
-	
-	
+		data = pickle.load(fp)
 	print(len(data))
-	print(len(data_2))
+	print(data[0])
+	
+	#----------------------
+	basic_info = ['name', 'rating', 'vote_count', 'instruction',
+			  'calories', 'energy', 'fats', 'carbohydrates', 'protein',
+			  'fiber', 'sugars', 'cholesterol', 'sodium', 'alcohol'] 
+	
+	name_set = set() # initialize a set holding names
+	for d in data:
+		for diction in d:
+			for k in diction:
+				name_set.add(k.lower())
+	print(len(name_set))
+	print(len(name_set - set(basic_info)))
+
+	#print(basic_info)
+	
+	
 	
 clean_data()
